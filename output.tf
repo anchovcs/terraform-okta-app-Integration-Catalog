0549bb50615926811f8fda34945f384c73a181d3
Anil Choubey <anilchoubey@Anils-MacBook-Air.local>
2026-02-16 12:49:35 +0000

    added secret output to TFC

--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
 output "app_id" {
   value = var.app_type == "saml" ? okta_app_saml.samlapp[0].id : okta_app_oauth.oidcapp[0].id
 }
+
+output "client_id" {
+  value = var.app_type == "oidc" ? okta_app_oauth.oidcapp[0].client_id : null
+}
+
+output "client_secret" {
+  value     = var.app_type == "oidc" ? okta_app_oauth.oidcapp[0].client_secret : null
+  sensitive = true
+}
